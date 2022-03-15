f62dd138b5c05be9046df60cdb417c49419115cc
msubaie <63135674+msubaie@users.noreply.github.com>
2022-03-15 14:43:46 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://732e8ec9.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
